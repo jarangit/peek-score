7e458a2df8b0cbdb7b415076892eda62ba47e54a
jarangit <jaran.dch@gmail.com>
2025-01-23 09:24:31 +0700

    initialize project with Vite, React, and TypeScript setup

--- src/App.tsx
+++ src/App.tsx
@@ -34,7 +34,6 @@ function App() {
   useEffect(() => {
     const getMatchData = async () => {
       const { response }: any = await fetchMatchData();
-
       setMatchData(response);
     };
     getMatchData();
